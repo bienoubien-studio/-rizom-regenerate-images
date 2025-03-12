7eb931ec6f90355125501ddad3e8fdf81ecbf4cc
bienoubien-studio <salut@bienoubien.studio>
2025-03-12 12:35:05 +0100

    Clean

--- src/hooks.server.ts
+++
@@ -1,6 +0,0 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import { handlers } from 'rizom';
-import config from './config/rizom.config.js';
-import * as schema from './lib/server/schema.js';
-
-export const handle = sequence(...handlers({ config, schema }));
